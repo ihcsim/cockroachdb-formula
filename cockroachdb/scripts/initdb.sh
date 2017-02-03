f77d9e4046edef6dc70e9476331e2072162ccb20
Ivan Sim <ihcsim@gmail.com>
2017-02-02 22:06:30 -0800

    Add default superuser and database.
    These will be created when CockroachDB is ready.

--- cockroachdb/scripts/initdb.sh
+++ cockroachdb/scripts/initdb.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# This shell script creates the database super user. (See default.yml for defaults.)
+# If an initdb.sql is present, it will be executed.
+
 {% from 'cockroachdb/map.jinja' import config with context %}
 
 until {{ config.home_dir }}/cockroach sql --insecure --execute='SELECT 1'; do
   echo 'Waiting for CockroachDB to come online...'
   sleep 2
 done
+
+{{ config.home_dir }}/cockroach user set {{ config.dbuser }}
+{{ config.home_dir }}/cockroach sql -e 'CREATE DATABASE {{ config.database }} IF NOT EXISTS'
+{{ config.home_dir }}/cockroach sql -e 'GRANT ALL ON DATABASE {{ config.database }} TO {{ config.dbuser }}'
